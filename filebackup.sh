762c4fb6f8866fe91e6934258f9b416de38980a9
Igor Matkovic <igor@cod3.me>
2015-08-01 21:54:03 +0100

    Allow multilevel Mkdir

--- filebackup.sh
+++ filebackup.sh
@@ -3,8 +3,7 @@
 backup_folder=`date '+%w'`
 
 
-mkdir /var/www/backup/files/$1/
-mkdir /var/www/backup/files/$1/${backup_folder}/
+mkdir -p /var/www/backup/files/$1/${backup_folder}/
 
 #Move files to the backup Folder
 rsync -arv /var/www/domains/$1 /var/www/backup/files/$1/${backup_folder}/
