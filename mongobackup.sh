41ebb4c75db6efa7420f19ee59181f549c86c446
Igor Matkovic <igor@cod3.me>
2015-08-01 18:56:27 +0100

    Add MongoBackup

---
+++ mongobackup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+backup_name=`date '+%w_%H'`
+
+
+mkdir /var/www/backup/tmp/$1
+
+#Dump DB to the current folder
+mongodump -d $1 -o /var/www/backup/tmp/$1
+
+#Tar it with the Date Name
+tar -zcvf /var/www/backup/db/$1/${backup_name}.tar.gz /var/www/backup/tmp/$1
